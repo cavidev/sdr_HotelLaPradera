5690583bba7dc35862d994568cb6ad554a1b4ea5
Leidy Chacón Salas <Leidy Chacn Salas>
2016-11-20 03:15:47 -0600

    avance del perfil de usuario

--- appWEB/User/servicio/usuario.js
+++ appWEB/User/servicio/usuario.js
@@ -1,7 +1,34 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+/*Desarrollador: Desarrollador: Carlos Villafuerte, Esteban Blanco, Lady Chacon, Roberto Salazar.
+    Version: 1.0
+    Module AngularJS: HotelLaPredera
+    Año: 2016
+    Descripción: route, para la navegación de las paginas en AngularJS.
+*/
+angular.module('HotelLaPradera')
+.factory('usuarioFactory', function($http){
+    //Arma la funcion y la inserta de una ves en el json.
+    var getData = function getData(datosV,callback){
+        console.log("Mae estoy en el factory");
+        //La direccion del archivo PHP.
+        var url = "http://localhost/Hotel_La_Pradera/Logica/conexion.php?Funcion=GetData";
+        //Datos que vienen por parametros.
+        var datos = {Nombre:"Villlafuerte"};   
+        //Peticion por debajo.
+        $http.post(url,datos).success(function(data){
+            callback(data);
+        });
+    };
+    
+    var credencialesUsuario = function credencialesUsuario(credenciales, callback){
+        var url = "http://localhost/Hotel_La_Pradera/Logica/conexion.php?Funcion=getCredenciales";
+        $http.post(url,credenciales).success(function(response){
+            callback(response);
+        });
+    };
+    
+    //Retorna el json con todas las funciones necesarias.
+    return {
+        getData: getData,
+        credencialesUsuario: credencialesUsuario
+    };
+});
